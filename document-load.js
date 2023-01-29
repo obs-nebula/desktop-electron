c7ae0604d210310729ed5fe2172a6d90871168b9
Helio Frota <00hf11@gmail.com>
2023-01-29 07:53:48 -0300

    fix: fix #4

--- document-load.js
+++ document-load.js
@@ -1,15 +1,21 @@
+const { Resource } = require('./node_modules/@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('./node_modules/@opentelemetry/semantic-conventions');
 const { ConsoleSpanExporter, SimpleSpanProcessor, WebTracerProvider } = require('./node_modules/@opentelemetry/sdk-trace-web');
 const { FetchInstrumentation } = require('./node_modules/@opentelemetry/instrumentation-fetch');
 const { ZoneContextManager } = require('./node_modules/@opentelemetry/context-zone');
 const { registerInstrumentations } = require('./node_modules/@opentelemetry/instrumentation');
 const { JaegerExporter } = require('./node_modules/@opentelemetry/exporter-jaeger');
 
-const provider = new WebTracerProvider();
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: process.env.npm_package_name
+});
+
+const provider = new WebTracerProvider({ resource: resource });
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 provider.addSpanProcessor(new SimpleSpanProcessor(new JaegerExporter()));
 
 provider.register({
-  contextManager: new ZoneContextManager(),
+  contextManager: new ZoneContextManager()
 });
 
 registerInstrumentations({
