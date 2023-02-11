9694736162099d436237a3b9bc6857f5f4f58630
Helio Frota <00hf11@gmail.com>
2023-02-10 21:37:18 -0300

    chore: exporting to otelcol

--- document-load.js
+++ document-load.js
@@ -1,18 +1,19 @@
 const { Resource } = require('./node_modules/@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('./node_modules/@opentelemetry/semantic-conventions');
-const { ConsoleSpanExporter, SimpleSpanProcessor, WebTracerProvider } = require('./node_modules/@opentelemetry/sdk-trace-web');
+const { SimpleSpanProcessor, WebTracerProvider } = require('./node_modules/@opentelemetry/sdk-trace-web');
 const { FetchInstrumentation } = require('./node_modules/@opentelemetry/instrumentation-fetch');
 const { ZoneContextManager } = require('./node_modules/@opentelemetry/context-zone');
 const { registerInstrumentations } = require('./node_modules/@opentelemetry/instrumentation');
-const { JaegerExporter } = require('./node_modules/@opentelemetry/exporter-jaeger');
+const { OTLPTraceExporter } = require('./node_modules/@opentelemetry/exporter-trace-otlp-http');
 
 const resource = new Resource({
   [SemanticResourceAttributes.SERVICE_NAME]: process.env.npm_package_name
 });
 
+const exporter = new OTLPTraceExporter();
+
 const provider = new WebTracerProvider({ resource: resource });
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-provider.addSpanProcessor(new SimpleSpanProcessor(new JaegerExporter()));
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
 provider.register({
   contextManager: new ZoneContextManager()
--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
-      webSecurity: false
+      contextIsolation: false
     }
   });
   win.loadFile('index.html');
